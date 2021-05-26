089ba310528d488cbd4d5230c48a5d68ee348098
Mai-okn <maitel57rad@gmail.com>
2021-05-26 15:01:23 +0900

    アクティブレコードの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:image,:description,:price,:category_id,:status_id,
+    :delivery_charge_id,:delivery_day_id,:prefectured_id).merge(user_id: current_user.id)
+  end
 end
