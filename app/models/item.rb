1ef6b64f395eb1791f49469ef307d92e3b6af24f
Mai-okn <maitel57rad@gmail.com>
2021-05-26 21:48:43 +0900

    アクティブハッシュの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,24 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivary_charge
+  belongs_to :delivery_day
+  belongs_to :prefecture
+
+  with_options presence: true do
+    validates :name
+    validates :descriotion
+    validates :category_id
+    validates :status_id
+    validates :delivery_charge_id
+    validates :delivery_day_id
+    validates :prefectured_id
+  end
+
+  validates :price, numericality:{greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
+
 end
