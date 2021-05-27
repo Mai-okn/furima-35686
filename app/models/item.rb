03cdd538a4ad84455a71bfaf0812ce53ab05a3ab
Mai-okn <maitel57rad@gmail.com>
2021-05-27 14:39:54 +0900

    修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,9 +14,10 @@ class Item < ApplicationRecord
     validates :name  
     validates :description
     validates :price, numericality:{greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999, message: "is out of setting range"}
-    validates :price, numericality:{ only_integer: true, message: "is invalid. Input half-width characters" }
   end
-
+  
+  validates :price, numericality:{ only_integer: true, message: "is invalid. Input half-width characters" }
+  
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :status_id
