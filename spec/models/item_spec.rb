5f697d44777e669d18f31891c668a5c2d0508490
Mai-okn <maitel57rad@gmail.com>
2021-05-27 13:05:59 +0900

    テストコード記述

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe '商品出品' do
+    context '商品出品ができる時' do
+      it '全ての値が正しく入力されていれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+
+    context '商品出品ができない時' do
+      #空だと出品できない
+      it 'imageが空だと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空だと出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'descriptionが空だと出品できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'categoryが未選択だと出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'statusが未選択だと出品できない' do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it 'delivery_chargeが未選択だと出品できない' do
+        @item.delivery_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
+      end
+      it 'prefectureが未選択だと出品できない' do
+        @item.prefectured_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefectured can't be blank")
+      end
+      it 'delivery_dayが未選択だと出品できない' do
+        @item.delivery_day_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank")
+      end
+      it 'priceが空だと出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      #priceのテスト
+      it 'priceが全角数字だと出品できない' do
+        @item.price = "３０００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid. Input half-width characters")
+      end
+      it 'priceが300円以上9999999円以下でないと出品できない' do
+        @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+    end
+  end
+end
