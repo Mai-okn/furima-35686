49f1fee1687c1f985b2611b9cad7ca2df374c295
Mai-okn <maitel57rad@gmail.com>
2021-05-30 22:34:02 +0900

    モデルの整理

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,14 +1,14 @@
 class Address < ApplicationRecord
 
   belongs_to :trade
-  
-  with_options presence: true do
-    validates :phone_number
-    validates :postal_code
-    validates :prefectured_id
-    validates :city
-    validates :address
-  end
+
+  #with_options presence: true do
+    #validates :phone_number
+    #validates :postal_code
+    #validates :prefectured_id
+    #validates :city
+    #validates :address
+  #end
 
 
 end
