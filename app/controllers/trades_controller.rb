b24312747c50a7e85c7d02b9e72e92a73d342b99
Mai-okn <maitel57rad@gmail.com>
2021-06-01 18:39:20 +0900

    trade_addressモデルとコントローラー作成

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,7 +1,46 @@
 class TradesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :sold_out_item
+
   def index
+    @trade_address = TradeAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @trade_address = TradeAddress.new(trade_params)
+    @item = Item.find(params[:item_id])
+    if @trade_address.valid?
+      pay_item
+      @trade_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+
+
+  private
+  
+  def trade_params
+    params.require(:trade_address).permit(:postal_code, :prefectured_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: trade_params[:token], 
+      currency: 'jpy'
+    )
+  end
+
+  def sold_out_item
+    @item = Item.find(params[:item_id])
+    if @item.trade.present? || current_user.id == @item.user_id 
+      redirect_to root_path 
+    end
+  end
+
+  
 end
--- app/models/trade_address.rb
+++ app/models/trade_address.rb
@@ -1,22 +1,21 @@
 class TradeAddress
   include ActiveModel::Model
-  attr_accessor :phone_number, :postal_code, :prefectured_id, :city, :address, :building, :trade, :item, :user, :token
+  attr_accessor :phone_number, :postal_code, :prefectured_id, :city, :address, :building, :item_id, :user_id, :token
 
 
   with_options presence: true do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 333, less_than_or_equal_to: 99999999, message: 'is invalid'}
-    validates :user_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :phone_number
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :phone_number, numericality:{ only_integer: true, message: "is invalid. Input only number" }
     validates :prefectured_id
     validates :city
     validates :address
+    validates :token
   end
-  validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
-
+  validates :prefectured_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :phone_number, format:{with: /\A\d{10}$|^\d{11}\z/, message: "is too short"}
 
   def save
     trade = Trade.create(item_id: item_id, user_id: user_id)
-    Address.create(postal_code: postal_code, prefectured_id: prefectured_id, city: city, address: address, building: building, trade_id: trade.id)
+    Address.create(phone_number: phone_number, postal_code: postal_code, prefectured_id: prefectured_id, city: city, address: address, building: building, trade_id: trade.id)
   end
 end
